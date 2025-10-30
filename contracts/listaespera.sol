61bc689c3b5924bc8fc2fdad363aca33d6c5a5a8
Alfonso Dou <alfonsodou@gmail.com>
2025-10-30 19:58:20 +0100

    Añadida función yaEstaInscrito y numeroEnLista

--- contracts/listaespera.sol
+++ contracts/listaespera.sol
@@ -4,12 +4,48 @@ pragma solidity ^0.8.10;
 import './tokenespera.sol';
 
 contract ListaEspera {
-    address public  owner = 0xF55301514c27489Fde7C9cd9A3EA8044E7E04579;
+    address     public  owner = 0xF55301514c27489Fde7C9cd9A3EA8044E7E04579;
     TokenEspera public token;
-
-    mapping (address => uint256) usuarios;
+    address[]   public inscritos;
+    bool        private bloqueado; // Semáforo para funciones críticas
 
     constructor(TokenEspera token_) {
         token = token_;
+    }
+
+    function yaEstaInscrito(address _usuario) public view returns (bool) {
+        require(_usuario != owner, "El propietario no puede estar en la lista");
+
+        for(uint256 i = 0; i < inscritos.length; i++) {
+            if (inscritos[i] == _usuario) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function numeroEnLista(address _usuario) public view returns (bool, uint256) {
+        require(_usuario != owner, "El propietario no puede estar en la lista");
+        
+        uint256 indice = 0;
+        bool encontrado = false;
+        for(uint256 i = 0; (i < inscritos.length) && (!encontrado); i++) {
+            if (inscritos[i] == _usuario) {
+                indice = i;
+                encontrado = true;
+                break;
+            }
+        }
+
+        return (encontrado, indice);
+    }
+
+    // Modificador para prevenir ataques de reentrada
+    modifier noReentrancy() {
+        if (bloqueado) revert();
+        bloqueado = true;
+        _;
+        bloqueado = false;
     }
 }
